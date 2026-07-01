e3cd6a2de7ff4b86cd17e3fa6346f493cb1822f9
horodetsky95-oss <horodetsky95@gmail.com>
2026-07-01 11:59:10 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'assistant-iphone-v2';
+const CACHE = 'assistant-iphone-v3';
 const ASSETS = ['./index.html', './manifest.webmanifest', './apple-touch-icon.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
